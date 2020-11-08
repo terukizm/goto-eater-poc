30134aec405e2d4bedc01e22206d515365153ead
KOIZUMI teruaki <1755900+terukizm@users.noreply.github.com>
2020-11-08 21:47:05 +0900

    fix: 雑にアイコンをGeoJSON別に出し分け

--- main.js
+++ main.js
@@ -4,25 +4,22 @@ map.on('load', function () {
   // アイコン画像設定
   // MEMO?: https://qiita.com/kkdd/items/0eb24549d10e875c1fa5
   map.loadImage('https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png', function (error, res) {
-    map.addImage('sample1', res);
-  });
-  map.loadImage('https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png', function (error, res) {
-    map.addImage('sample2', res);
+    map.addImage('sample1_icon', res);
   });
 
   // アイコン設定
-  map.addSource('symbol_sample', {
+  map.addSource('sample1_datasource', {
     type: 'geojson',
-    data: '/geojson/sample.geojson'
+    data: '/geojson/sample1.geojson'
   });
 
   // スタイル設定
   map.addLayer({
-    "id": "symbol_sample",
+    "id": "layer1",
     "type": "symbol",
-    "source": "symbol_sample",
+    "source": "sample1_datasource",
     "layout": {
-      "icon-image": "sample1",
+      "icon-image": "sample1_icon",
       "icon-allow-overlap": true,
       "icon-size": 1.00
     },
@@ -30,7 +27,7 @@ map.on('load', function () {
   });
 
   // アイコンクリックイベント
-  map.on('click', "symbol_sample", function (e) {
+  map.on('click', "layer1", function (e) {
     const coordinates = e.lngLat;
     const props = e.features[0].properties;
 
@@ -51,11 +48,41 @@ map.on('load', function () {
   });
 
   // ポイントにマウスホバーさせたときにマウスカーソルを変化させる
-  map.on('mouseenter', 'symbol_sample', function () {
+  map.on('mouseenter', 'layer1', function () {
     map.getCanvas().style.cursor = 'pointer';
   });
-  map.on('mouseleave', 'symbol_sample', function () {
+  map.on('mouseleave', 'layer1', function () {
     map.getCanvas().style.cursor = '';
   });
+
+  //////////////////////////////////////////////////////////////////////////////
+  // 同様にdatasource2を読み込み
+  // TODO: 汎用的に
+
+  // 地図アイコン
+  map.loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png', function (error, res) {
+    map.addImage('sample2_icon', res);
+  });
+
+  // GEOJSON
+  map.addSource('sample2_datasource', {
+    type: 'geojson',
+    data: '/geojson/sample2.geojson'
+  });
+
+  // レイヤー指定
+  map.addLayer({
+    "id": "layer2",
+    "type": "symbol",
+    "source": "sample2_datasource",
+    "layout": {
+      "icon-image": "sample2_icon",
+      "icon-allow-overlap": true,
+      "icon-size": 0.1
+    },
+    "paint": {}
+  });
+
+  // 以下同様
 
 });
